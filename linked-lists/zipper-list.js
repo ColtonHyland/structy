77798b34cf557d558eff68d9270dee97da9d6fa1
Colton Hyland <colthyland@gmail.com>
2023-07-01 13:28:05 -0700

    recursive soln

--- linked-lists/zipper-list.js
+++ linked-lists/zipper-list.js
@@ -36,30 +36,50 @@
 //   }
 // }
 
-const zipperLists = (head1, head2) => {
-  let tail = head1;
-  let curr1 = head1.next;
-  let curr2 = head2;
-  let count = 0;
-  while (curr1 !== null && curr2 !== null) {
-    if(count % 2 === 0) {
-      tail.next = curr2;
-      curr2 = curr2.next;
-    } else {
-      tail.next = curr1;
-      curr1 = curr1.next;
-    }
-    tail = tail.next;
-    count++;
-  }
-  if(curr1 === null) tail.next = curr2;
-  if(curr2 === null) tail.next = curr1;
-  return head1;
-};
+//n = length of list 1
+// m = length of list 2
+// Time: O(min(n, m))
+// Space: O(1)
 
-module.exports = {
-  zipperLists,
-};
+// //iterative soln
+// const zipperLists = (head1, head2) => {
+//   let tail = head1;
+//   let curr1 = head1.next;
+//   let curr2 = head2;
+//   let count = 0;
+//   while (curr1 !== null && curr2 !== null) {
+//     if(count % 2 === 0) {
+//       tail.next = curr2;
+//       curr2 = curr2.next;
+//     } else {
+//       tail.next = curr1;
+//       curr1 = curr1.next;
+//     }
+//     tail = tail.next;
+//     count++;
+//   }
+//   if(curr1 === null) tail.next = curr2;
+//   if(curr2 === null) tail.next = curr1;
+//   return head1;
+// };
+
+
+// recursive soln
+// const zipperLists = (head1, head2) => {
+//   if(head1 === null && head2 === null) return null;
+//   if(head1 === null) return head2;
+//   if(head2 === null) return head1;
+//   let next1 = head1.next;
+//   let next2 = head2.next;
+//   head1.next = head2;
+//   head2.next = zipperLists(next1, next2);
+//   return head1;
+// };
+
+//n = length of list 1
+// m = length of list 2
+// Time: O(min(n, m))
+// Space: O(min(n, m))
 
 
 module.exports = {
