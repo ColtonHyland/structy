c2551cb15faa162d68a42d7223f81f733bd61f36
Colton Hyland <colthyland@gmail.com>
2023-07-01 13:12:52 -0700

    Iterative soln

--- linked-lists/zipper-list.js
+++ linked-lists/zipper-list.js
@@ -37,8 +37,30 @@
 // }
 
 const zipperLists = (head1, head2) => {
-  // todo
+  let tail = head1;
+  let curr1 = head1.next;
+  let curr2 = head2;
+  let count = 0;
+  while (curr1 !== null && curr2 !== null) {
+    if(count % 2 === 0) {
+      tail.next = curr2;
+      curr2 = curr2.next;
+    } else {
+      tail.next = curr1;
+      curr1 = curr1.next;
+    }
+    tail = tail.next;
+    count++;
+  }
+  if(curr1 === null) tail.next = curr2;
+  if(curr2 === null) tail.next = curr1;
+  return head1;
 };
+
+module.exports = {
+  zipperLists,
+};
+
 
 module.exports = {
   zipperLists,
