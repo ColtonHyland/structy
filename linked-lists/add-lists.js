a4cb77f5f43f9c568c9c6bb75a89b6c652bc6df7
Colton Hyland <colthyland@gmail.com>
2023-07-02 13:49:16 -0700

    recusrive soln

--- linked-lists/add-lists.js
+++ linked-lists/add-lists.js
@@ -48,9 +48,25 @@ class Node {
   }
 }
 
-const addLists = (head1, head2) => {
-  // todo
-};
+// recursive soln
+// const addLists = (head1, head2, carry = 0) => {
+//   if(head1 === null && head2 === null && carry === 0) return null;
+  
+//   const val1 = head1 === null ? 0 : head1.val;
+//   const val2 = head2 === null ? 0 : head2.val;
+  
+//   const sum = val1 + val2 + carry;
+//   const nextCarry = sum > 9 ? 1 : 0;
+//   const digit = sum % 10;
+  
+//   const resultNode = new Node(digit);
+//   const next1 = head1 === null ? null : head1.next;
+//   const next2 = head2 === null ? null : head2.next;
+  
+  
+//   resultNode.next = addLists(next1, next2, nextCarry);
+//   return resultNode;
+// };
 
 module.exports = {
   addLists,
