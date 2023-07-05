3b60051160b38a21611050607afaddf93d84c93a
Colton Hyland <colthyland@gmail.com>
2023-07-04 20:47:15 -0700

    Iterative soln

--- linked-lists/add-lists.js
+++ linked-lists/add-lists.js
@@ -48,6 +48,31 @@ class Node {
   }
 }
 
+// iterative soln
+// const addLists = (head1, head2) => {
+//   const dummyHead = new Node(null)
+//   let tail = dummyHead;
+//   let current1 = head1;
+//   let current2 = head2;
+//   let carry = 0;
+//   while(current1 !== null || current2 !== null || carry !== 0) {
+//     const val1 = current1 !== null ? current1.val : 0;
+//     const val2 = current2 !== null ? current2.val : 0;
+//     const sum = val1 + val2 + carry;
+//     carry = sum > 9 ? 1 : 0;
+//     const digit = sum % 10;
+//     tail.next = new Node(digit);
+//     tail = tail.next
+//     if(current1 !== null) current1 = current1.next;
+//     if(current2 !== null) current2 = current2.next;
+//   }
+//   return dummyHead.next;
+// };
+// n = length of list 1
+// m = length of list 2
+// Time: O(max(n, m))
+// Space: O(max(n, m))
+
 // recursive soln
 // const addLists = (head1, head2, carry = 0) => {
 //   if(head1 === null && head2 === null && carry === 0) return null;
