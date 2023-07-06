48aacaff4f9f8654a506ecdb0bd7eb0775c541f6
Colton Hyland <colthyland@gmail.com>
2023-07-05 19:37:37 -0700

    Iterative soln

--- binary-tree/tree-sum.js
+++ binary-tree/tree-sum.js
@@ -30,9 +30,19 @@
 //   }
 // }
 
-const treeSum = (root) => {
-  // todo
-};
+// // iterative soln
+// const treeSum = (root) => {
+//   if(root === null) return 0;
+//   let sum = 0;
+//   const stack = [ root ]
+//   while(stack.length > 0) {
+//     const current = stack.pop();
+//     sum += current.val;
+//     if(current.right !== null) stack.push(current.right);
+//     if(current.left !== null) stack.push(current.left);
+//   }
+//   return sum;
+// }
 
 module.exports = {
   treeSum,
