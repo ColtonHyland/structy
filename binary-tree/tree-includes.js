8b837a3aec8a21762c21e181522a8912dff6c375
Colton Hyland <colthyland@gmail.com>
2023-07-05 20:23:13 -0700

    Tree min value init

--- binary-tree/tree-includes.js
+++ binary-tree/tree-includes.js
@@ -21,3 +21,43 @@
 // d   e     f
 
 // treeIncludes(a, "e"); // -> true
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+// depth-first iterative soln
+// const treeIncludes = (root, target) => {
+//   if(root === null) return false;
+//   const stack = [ root ];
+//   while(stack.length > 0) {
+//     const current = stack.pop();
+//     if(current.val === target) return true;
+//     if(current.left !== null) stack.push(current.left);
+//     if(current.right !== null) stack.push(current.right);
+//   }
+//   return false;
+// };
+// n = number of nodes
+// Time: O(n)
+// Space: O(n)
+// Note: this solution should really be considered O(n^2) runtime because the JavaScript shift() methods runs in O(n).
+//  JavaScript does not have a native queue data structure that is maximally efficient.
+
+// depth first recursive soln
+// const treeIncludes = (root, target) => {
+//   if(root === null) return false;
+//   if(root.val === target) return true;
+//   return treeIncludes(root.left, target) || treeIncludes(root.right, target); 
+// };
+// n = number of nodes
+// Time: O(n)
+// Space: O(n)
+
+// module.exports = {
+//   treeIncludes,
+// };
