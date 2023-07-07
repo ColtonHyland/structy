0c1ad66c736bb338680595340f8c5dae139e0056
Colton Hyland <colthyland@gmail.com>
2023-07-07 12:11:25 -0700

    Recursive soln

--- binary-tree/how-high.js
+++ binary-tree/how-high.js
@@ -26,3 +26,23 @@
 
 // howHigh(a); // -> 2
 
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+const howHigh = (node) => {
+  if(node === null) return -1;
+  return 1 + Math.max(howHigh(node.left), howHigh(node.right));
+};
+
+// n = number of nodes
+// Time: O(n)
+// Space: O(n)
+
+module.exports = {
+  howHigh,
+};
