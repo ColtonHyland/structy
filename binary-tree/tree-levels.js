71f7cfafc16a437a5cf0b792729b31336a933595
Colton Hyland <colthyland@gmail.com>
2023-07-08 16:48:25 -0700

    Recursive solns

--- binary-tree/tree-levels.js
+++ binary-tree/tree-levels.js
@@ -62,6 +62,54 @@
 //   return levels;
 // };
 
+// breadth first iterative
+// const treeLevels = (root) => {
+//   if (root === null) return [];
+
+//   const levels = [];
+//   const queue = [{ node: root, levelNum: 0 }];
+//   while (queue.length > 0) {
+//     const { node, levelNum } = queue.shift();
+
+//     if (levels.length === levelNum) {
+//       levels[levelNum] = [node.val];
+//     } else {
+//       levels[levelNum].push(node.val);
+//     }
+
+//     if (node.left !== null) queue.push({ node: node.left, levelNum: levelNum + 1 });
+//     if (node.right !== null) queue.push({ node: node.right, levelNum: levelNum + 1 });
+//   }
+
+//   return levels;
+// };
+// n = number of nodes
+// Time: O(n)
+// Space: O(n)
+
+// revursive soln
+// const treeLevels = (root) => {
+//   const levels = [];
+//   treeLevelsHelper(root, levels, 0);
+//   return levels;
+// };
+
+// const treeLevelsHelper = (root, levels, level) => {
+//   if(root === null) return;
+  
+//   if(levels.length === level) {
+//     levels.push([ root.val ]);
+//   } else {
+//     levels[level].push(root.val);
+//   }
+  
+//   treeLevelsHelper(root.left, levels, level + 1);
+//   treeLevelsHelper(root.right, levels, level + 1);
+// };
+// n = number of nodes
+// Time: O(n)
+// Space: O(n)
+
 module.exports = {
   treeLevels,
 };
