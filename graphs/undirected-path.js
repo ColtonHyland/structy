e206df2929f58828c00f2f6bcfbfdc065138a28c
Colton Hyland <colthyland@gmail.com>
2023-07-10 17:42:08 -0700

    depth first recursive soln

--- graphs/undirected-path.js
+++ graphs/undirected-path.js
@@ -2,13 +2,36 @@
 // and two nodes (nodeA, nodeB). The function should return a boolean indicating whether or
 // not there exists a path between nodeA and nodeB.
 
+// depth first
+// n = number of nodes
+// e = number edges
+// Time: O(e)
+// Space: O(e)
 const undirectedPath = (edges, nodeA, nodeB) => {
+  const graph = buildGraph(edges);
+  return hasPath(graph, nodeA, nodeB, new Set());
+};
+
+const buildGraph = (edges) => {
   const graph = {};
   for (let edge of edges) {
-    graph[edge[0]] = edge[1];
-    graph[edge[1]] = edge[0];
+    const [ a, b ] = edge;
+    if (!(a in graph)) graph[a] = [];
+    if (!(b in graph)) graph[b] = [];
+    graph[a].push(b);
+    graph[b].push(a);
   }
-  console.log(graph)
+  return graph;
+};
+
+const hasPath = (graph, src, dst, visited) => {
+  if (src === dst) return true;
+  if (visited.has(src)) return false;
+  visited.add(src);
+  for (let neighbor of graph[src]) {
+    if (hasPath(graph, neighbor, dst, visited) === true) return true;
+  }
+  return false;
 };
 
 module.exports = {
