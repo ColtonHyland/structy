402c9e9aa6d2ec0788dec0a9691bc025b37b63bd
Colton Hyland <colthyland@gmail.com>
2023-07-13 10:14:10 -0700

    Breath first traversal

--- graphs/shortest-path.js
+++ graphs/shortest-path.js
@@ -14,9 +14,42 @@
 
 //shortestPath(edges, 'w', 'z'); // -> 2
 
-const shortestPath = (edges, nodeA, nodeB) => {
 
+//breadth frist traversal
+// e = number edges
+// Time: O(e)
+// Space: O(e)
+
+const shortestPath = (edges, nodeA, nodeB) => {
+  const graph = createGraph(edges);
+  const visited = new Set();
+  const queue = [ [nodeA, 0] ];
+  while (queue.length > 0) {
+    const [ current, distance ] = queue.shift();
+    if (current === nodeB) return distance;
+    
+    for (let neighbor of graph[current]) {
+      if (!visited.has(neighbor)) {
+        visited.add(neighbor);
+        queue.push([neighbor, distance + 1]);
+      }
+    }
+  }
+  return -1;
 };
+
+const createGraph = (edges) => {
+  const graph = {};
+  for (let edge of edges) {
+    const [ a, b ] = edge;
+    if (!(a in graph)) graph[a] = [];
+    if (!(b in graph)) graph[b] = [];
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+  return graph;
+};
+
 
 module.exports = {
   shortestPath,
