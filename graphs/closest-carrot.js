8975ccbb9eaaf31c0da57e900d4555397ad8cf90
Colton Hyland <colthyland@gmail.com>
2023-07-13 19:29:09 -0700

    Iterative soln

--- graphs/closest-carrot.js
+++ graphs/closest-carrot.js
@@ -15,3 +15,33 @@
 // ];
 
 // closestCarrot(grid, 1, 2); // -> 4
+
+const closestCarrot = (grid, startRow, startCol) => {
+  const visited = new Set([ startRow + ',' + startCol])
+  const queue = [ [ startRow, startCol, 0 ] ];
+  while (queue.length > 0) {
+    const [ row, col, dist ] = queue.shift();
+    
+    if(grid[row][col] === 'C') return dist;
+    
+    const deltas = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+    
+    for (let delta of deltas) {
+      const [ rowDelta, colDelta ] = delta;
+      const neighborRow = row + rowDelta;
+      const neighborCol = col + colDelta;
+      const rowInbounds = 0 <= neighborRow && neighborRow < grid.length;
+      const colInbounds = 0 <= neighborCol && neighborCol < grid[0].length;
+      const pos = neighborRow + ',' + neighborCol;
+      if (rowInbounds && colInbounds && grid[neighborRow][neighborCol] !== 'X' && !visited.has(pos)) {
+        queue.push( [neighborRow, neighborCol, dist + 1]);
+        visited.add(pos);
+      }
+    }
+  }
+  return -1;
+};
+
+module.exports = {
+  closestCarrot,
+};
