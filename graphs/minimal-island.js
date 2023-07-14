2a0e0302f1fbb30fdac6175c55d2329a9f575dff
Colton Hyland <colthyland@gmail.com>
2023-07-13 19:12:48 -0700

    Iterative & recursive soln

--- graphs/minimal-island.js
+++ graphs/minimal-island.js
@@ -16,8 +16,41 @@
 
 // minimumIsland(grid); // -> 2
 
+// r = number of rows
+// c = number of columns
+// Time: O(rc)
+// Space: O(rc)
+
 const minimumIsland = (grid) => {
-  // todo
+  let smallest = Infinity;
+  const visited = new Set();
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[0].length; col++) {
+      const islandSize = exploreGrid(grid, row, col, visited);
+      if (islandSize > 0 && islandSize < smallest) smallest = islandSize;
+    }
+  }
+  return smallest;
+};
+
+const exploreGrid = (grid, row, col, visited) => {
+  const rowInbounds = 0 <= row && row < grid.length;
+  const colInbounds = 0 <= col && col < grid[0].length;
+  if (!rowInbounds || !colInbounds) return 0;
+  
+  if(grid[row][col] === 'W') return 0;
+  
+  const pos = row + ',' + col;
+  if (visited.has(pos)) return 0;
+  visited.add(pos);
+  
+  let size = 1;
+  size += exploreGrid(grid, row - 1, col, visited);
+  size += exploreGrid(grid, row + 1, col, visited);
+  size += exploreGrid(grid, row, col - 1, visited);
+  size += exploreGrid(grid, row, col + 1, visited);
+  
+  return size;
 };
 
 module.exports = {
