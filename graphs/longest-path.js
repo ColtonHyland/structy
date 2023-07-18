edb5d495da437c03af06998872ff8e26a61c3818
Colton Hyland <colthyland@gmail.com>
2023-07-17 21:35:24 -0700

    Depth first soln

--- graphs/longest-path.js
+++ graphs/longest-path.js
@@ -12,8 +12,36 @@
 
 // longestPath(graph); // -> 2
 
+// e = # edges
+// n = # nodes
+// Time: O(e)
+// Space: O(n
 const longestPath = (graph) => {
-  // todo
+  const distance = {};
+  for (let node in graph) {
+    if (graph[node].length === 0) {
+      distance[node] = 0;
+    }
+  }
+  
+  for (let node in graph) {
+    traverseDistance(graph, node, distance)
+  }
+  
+  return Math.max(...Object.values(distance));
+};
+
+const traverseDistance = (graph, node, distance) => {
+  if (node in distance) return distance[node];
+  
+  let maxLength = 0;
+  for (let neighbor of graph[node]) {
+    const attempt = traverseDistance(graph, neighbor, distance);
+    if (attempt > maxLength) maxLength = attempt;
+  }
+  
+  distance[node] = maxLength + 1;
+  return distance[node];
 };
 
 module.exports = {
