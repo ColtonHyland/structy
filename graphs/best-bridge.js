8542dd9a0b991cf8c326b7be61ac62b0fc6f1903
Colton Hyland <colthyland@gmail.com>
2023-10-17 12:47:14 -0700

    best bridge init

---
+++ graphs/best-bridge.js
@@ -0,0 +1,23 @@
+// best bridge
+// Write a function, bestBridge, that takes in a grid as an argument. The grid contains water (W) and land (L).
+//  There are exactly two islands in the grid. An island is a vertically or horizontally connected region of land.
+//   Return the minimum length bridge needed to connect the two islands.
+//  A bridge does not need to form a straight line.
+
+// const grid = [
+//   ["W", "W", "W", "L", "L"],
+//   ["L", "L", "W", "W", "L"],
+//   ["L", "L", "L", "W", "L"],
+//   ["W", "L", "W", "W", "W"],
+//   ["W", "W", "W", "W", "W"],
+//   ["W", "W", "W", "W", "W"],
+// ];
+// bestBridge(grid); // -> 1
+
+const bestBridge = (grid) => {
+  // todo
+};
+
+module.exports = {
+  bestBridge,
+};
